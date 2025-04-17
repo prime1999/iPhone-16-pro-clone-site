a59163e49674de5cf77849810ef897fd9642b615
prime <primetechnologies02@gmail.com>
2025-04-17 12:43:14 +0100

    fixed more styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,18 +29,21 @@ const Hero = () => {
 	}, []);
 	return (
 		<>
-			<section className="w-full relative">
+			<section className="w-full relative bg-black h-[650px]">
 				<div
 					ref={navBarRef}
 					className="fixed top-0 left-0 right-0 bg-gray-300 backdrop-blur-[10px] shadow-lg z-[999] border-b border-gray -translate-y-[100%]"
 				>
 					<OnScollNav />
 				</div>
-				<div className="flex items-center justify-center trigger">
+				<div className="relative w-full h-full flex items-center justify-center trigger">
+					<h1 className="text-3xl text-center text-white font-semibold z-10 hidden md:block">
+						iPhone 16 Pro
+					</h1>
 					<img
 						src={heroImg}
 						alt="big phone image"
-						className="py-12 max-sm:hidden"
+						className="absolute top-0 left-0 w-full h-full py-12 z-30 max-sm:hidden"
 					/>
 					<div className="flex flex-col items-center justify-center py-24 md:hidden">
 						<h1 className="text-white text-2xl font-semibold">IPhone 16 Pro</h1>
